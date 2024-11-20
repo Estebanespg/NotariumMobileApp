688f63230eef453b0669d4a13ec29a9aba94b87c
Esteban <estebanespg1@gmail.com>
2024-11-20 17:34:35 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import icon from './assets/ic_notarium.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={icon} style={{ width: 100, height: 100 }} />
+      <Text>Notarium</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Guardar</Text>
+      </Pressable>
     </View>
   );
 }
@@ -16,5 +21,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
