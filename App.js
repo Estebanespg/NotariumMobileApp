5d87dfa14d66aae28dc42834d7323d30a5b8ff96
Esteban <estebanespg1@gmail.com>
2024-11-21 00:00:34 -0500

    login screen

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image, Pressable } from 'react-native';
-import icon from './assets/ic_notarium_light.png';
+import { View } from 'react-native';
+import { Login } from './components/Login';
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <StatusBar style="auto" />
-      <Image source={icon} style={{ width: 100, height: 100 }} />
-      <Text className="text-green-600 text-4xl">Notarium</Text>
-      <Pressable  className="bg-black items-center justify-center py-3 px-8 rounded">
-        <Text className="color-white text-lg font-bold tracking-wide">Guardar</Text>
-      </Pressable>
+    <View className="flex-1 bg-[#11181d] items-center justify-center px-5">
+      <StatusBar style="light" />
+      <Login />
     </View>
   );
 }
