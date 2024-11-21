ff8fc583e0f98c8fb360834ddb20d4f4dd7cd79a
Esteban <estebanespg1@gmail.com>
2024-11-20 21:26:52 -0500

    stylesheet correction

--- App.js
+++ App.js
@@ -1,42 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { Text, View, Image, Pressable } from 'react-native';
 import icon from './assets/ic_notarium_light.png';
-import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-white items-center justify-center">
       <StatusBar style="auto" />
       <Image source={icon} style={{ width: 100, height: 100 }} />
-      <Text className="text-orange-600 ">Notarium</Text>
-      <Pressable style={styles.button}>
-        <Text style={styles.text}>Guardar</Text>
+      <Text className="text-green-600 text-4xl">Notarium</Text>
+      <Pressable  className="bg-black items-center justify-center py-3 px-8 rounded">
+        <Text className="color-white text-lg font-bold tracking-wide">Guardar</Text>
       </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'black',
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-});
