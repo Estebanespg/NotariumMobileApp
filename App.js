6f7cd697f54e92c053f9153131e557b4a74a40fb
Esteban <estebanespg1@gmail.com>
2024-11-22 01:39:34 -0500

    Little improvements :D

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import * as NavigationBar from 'expo-navigation-bar';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { Login } from './components/Login';
 
 export default function App() {
+  NavigationBar.setBackgroundColorAsync("#11181d");
   return (
     <View className="flex-1 bg-[#11181d] items-center justify-center px-5">
       <StatusBar style="light" />
--- components/Login.jsx
+++ components/Login.jsx
@@ -31,20 +31,18 @@ export function Login() {
         <TextInput style={{ fontFamily: 'Sora_400Regular' }} className="placeholder:text-slate-600 color-white h-full text-lg" placeholder="Usuario" keyboardType="default"></TextInput>
       </View>
 
-      <View className="flex-row items-center py-1/2 pl-4 w-full bg-[#11181d] border-2 border-slate-600 rounded-2xl px-3 mb-5">
+      <View className="flex-row items-center py-1/2 pl-4 w-full bg-[#11181d] border-2 border-slate-600 rounded-2xl px-3 mb-10">
         <FontAwesome className="mr-2" name="lock" size={22} color="white" />
         <TextInput style={{ fontFamily: 'Sora_400Regular' }} className="placeholder:text-slate-600 color-white h-full text-lg" placeholder="Contraseña" secureTextEntry></TextInput>
       </View>
 
-      {/* <Pressable className="w-full h-14 bg-[#1E90FF] border-none rounded-lg items-center justify-center mt-5 mb-5">
-        <Text className="color-white text-xl font-bold">Guardar</Text>
-      </Pressable> */}
-
-      <Pressable className="w-full h-14 bg-indigo-700 border-none rounded-lg items-center justify-center mt-5 mb-5">
-        <Text style={{ fontFamily: 'Sora_700Bold' }} className="color-white text-xl">Guardar</Text>
+      <Pressable className="w-full h-14 bg-[#6440a5] border-none rounded-lg items-center justify-center mb-5">
+        <Text style={{ fontFamily: 'Sora_500Medium' }} className="color-white text-lg">Guardar</Text>
       </Pressable>
 
-      <Text style={{ fontFamily: 'Sora_500Medium' }} className="text-white font-medium text-lg mb-8">No tienes una cuenta? <Text style={{ fontFamily: 'Sora_700Bold' }} className="color-[#1E90FF]">Regístrate</Text></Text>
+      <Text style={{ fontFamily: 'Sora_500Medium' }} className="text-white font-medium">No tienes una cuenta? <Text style={{ fontFamily: 'Sora_700Bold' }} className="color-[#1E90FF]">Regístrate</Text></Text>
+
+      <Text style={{ fontFamily: 'Sora_600SemiBold' }} className="absolute bottom-2.5 left-0 right-0 text-center color-white tracking-wide">Notarium</Text>
     </>
   )
 }
