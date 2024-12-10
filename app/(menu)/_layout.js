22d6595af05185a3e6ef4f4bb18062fc7dd66d6b
Esteban <estebanespg1@gmail.com>
2024-12-10 00:11:05 -0500

    navigation header style

--- app/(menu)/_layout.js
+++ app/(menu)/_layout.js
@@ -3,14 +3,46 @@ import { StatusBar } from 'expo-status-bar';
 import { Stack } from 'expo-router';
 import "../../global.css";
 import { AuthContextProvider } from '../../context/AuthContext';
+import { useFonts } from 'expo-font';
+import { Sora_100Thin, Sora_200ExtraLight, Sora_300Light, Sora_400Regular, Sora_500Medium, Sora_600SemiBold, Sora_700Bold, Sora_800ExtraBold } from '@expo-google-fonts/sora';
+import ScreenLayout from '../../components/ScreenLayout';
+import { View, Image, Text } from 'react-native';
+import icon from '../../assets/ic_notarium_light_white.png';
 
 export default function MenuLayout() {
+  const [fontsLoaded] = useFonts({
+    Sora_100Thin,
+    Sora_200ExtraLight,
+    Sora_300Light,
+    Sora_400Regular,
+    Sora_500Medium,
+    Sora_600SemiBold,
+    Sora_700Bold,
+    Sora_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <ScreenLayout><Text className="color-white">Cargando fuentes...</Text></ScreenLayout>;
+  }
+
   NavigationBar.setBackgroundColorAsync("#11181d");
   return (
     <AuthContextProvider>
       <>
         <StatusBar style="light" />
-        <Stack />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: "#11181d" },
+            headerTintColor: "white",
+            title: "",
+            headerTitle: () =>
+              <View className="flex-row items-center">
+                <Image className="w-10 h-10" source={icon} />
+                <Text style={{ fontFamily: 'Sora_600SemiBold' }} className="color-white tracking-wide ml-2">Notarium</Text>
+              </View>,
+            // headerLeft: () => <Image className="w-10 h-10 mt-5" source={icon} />,
+          }}
+        />
       </>
     </AuthContextProvider>
 
