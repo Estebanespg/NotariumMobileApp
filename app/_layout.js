e163046cb3876670cc7ec05e5b710993a8b747ad
Esteban <estebanespg1@gmail.com>
2024-12-19 19:12:42 -0500

    toast info, success, error styles modified

--- app/_layout.js
+++ app/_layout.js
@@ -3,16 +3,84 @@ import { StatusBar } from 'expo-status-bar';
 import { Slot } from 'expo-router';
 import "../global.css";
 import { AuthContextProvider } from '../context/AuthContext';
-import ToastManager from 'toastify-react-native';
+import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
+
+const toastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{
+        borderLeftColor: 'green',
+        borderLeftWidth: 7,
+        width: '90%',
+        height: '70',
+        borderRightColor: 'green',
+        borderRightWidth: 7,
+      }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: '700'
+      }}
+      text2Style={{
+        fontSize: 14,
+      }}
+    />
+  ),
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      text2NumberOfLines={3}
+      style={{
+        borderLeftColor: '#EF4444',
+        borderLeftWidth: 7,
+        width: '90%',
+        height: '70',
+        borderRightColor: '#EF4444',
+        borderRightWidth: 7,
+      }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: '700'
+      }}
+      text2Style={{
+        fontSize: 14
+      }}
+    />
+  ),
+  info: (props) => (
+    <ErrorToast
+      {...props}
+      text2NumberOfLines={3}
+      style={{
+        borderLeftColor: '#6440a5',
+        borderLeftWidth: 7,
+        width: '90%',
+        height: '70',
+        borderRightColor: '#6440a5',
+        borderRightWidth: 7,
+      }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: '700'
+      }}
+      text2Style={{
+        fontSize: 14
+      }}
+    />
+  )
+};
 
 export default function Layout() {
   NavigationBar.setBackgroundColorAsync("#11181d");
   return (
     <AuthContextProvider>
       <>
-        <ToastManager />
         <StatusBar style="light" />
         <Slot />
+        <Toast config={toastConfig} />
       </>
     </AuthContextProvider>
   )
