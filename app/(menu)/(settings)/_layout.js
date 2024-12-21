751f8e53b9506b2cce6e9b3c81bcc7081a468a0a
Esteban <estebanespg1@gmail.com>
2024-12-21 04:16:55 -0500

    tab navigation done

--- app/(menu)/(settings)/_layout.js
+++ app/(menu)/(settings)/_layout.js
@@ -1,18 +1,28 @@
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function SettingsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: '#11181d' },
+        tabBarActiveTintColor: "#6440a5",
+        
+      }}
+    >
       <Tabs.Screen
         name="Students"
         options={{
-          title: 'Menú'
+          title: 'Menú',
+          tabBarIcon: ({ color }) => <FontAwesome name="home" size={26} color={color} />
         }}
       />
       <Tabs.Screen
         name="Settings"
         options={{
-          title: 'Configuración'
+          title: 'Configuración',
+          tabBarIcon: ({ color }) => <FontAwesome name="gear" size={26} color={color} />
         }}
       />
     </Tabs>
