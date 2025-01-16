8bcc34f40f52b5db53e1a2572c4faf02cc1f8f0d
Esteban <estebanespg1@gmail.com>
2025-01-16 01:17:43 -0500

    firebase credentials in dotenv

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 import { initializeApp } from "firebase/app";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
@@ -6,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsM06VPTslIARKZpUOB_lhrR54lj8-d3g",
-  authDomain: "test-2be2e.firebaseapp.com",
-  projectId: "test-2be2e",
-  storageBucket: "test-2be2e.firebasestorage.app",
-  messagingSenderId: "126861830121",
-  appId: "1:126861830121:web:9f1c8ba6d0469f601fcd78"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
